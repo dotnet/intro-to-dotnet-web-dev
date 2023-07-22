fc122d41a7e1719b6d68343ac629bf018fa4034a
Isaac Alter <i_alter@yahoo.com>
2023-07-22 02:28:45 -0600

    Add feat: Debugging testing

---
+++ 2-csharp/lesson-6-projects/Debug101/Program.cs
@@ -0,0 +1,34 @@
+﻿/* 
+This code uses a names array and corresponding methods to display
+greeting messages
+*/
+
+string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+
+string messageText = "";
+
+foreach (string name in names)
+{
+    if (name == "Sophia")
+        messageText = SophiaMessage();
+    else if (name == "Andrew")
+        messageText = AndrewMessage();
+    else if (name == "AllGreetings")
+        messageText = SophiaMessage();
+        messageText = messageText + "\n\r" + AndrewMessage();
+
+    Console.WriteLine(messageText + "\n\r");
+}
+
+bool pauseCode = true;
+while (pauseCode == true);
+
+static string SophiaMessage()
+{
+    return "Hello, my name is Sophia.";
+}
+
+static string AndrewMessage()
+{
+    return "Hi, my name is Andrew. Good to meet you.";
+}
